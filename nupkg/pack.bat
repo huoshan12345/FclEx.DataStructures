5d729ec293cebfa2af2cf16623293dd61fee13c0
huoshan12345 <89009143@qq.com>
2016-11-07 11:43:41 +0800

    pack

--- nupkg/pack.bat
+++ nupkg/pack.bat
@@ -1,4 +1,4 @@
-REM "..\tools\gitlink\GitLink.exe" ..\ -u https://github.com/huoshan12345/FxUtility -c release
+REM "..\tools\gitlink\GitLink.exe" ..\ -u https://github.com/huoshan12345/FxUtility.DataStructures.git -c release
 
 @ECHO OFF
 SET /P VERSION_SUFFIX=Please enter version-suffix (can be left empty): 
