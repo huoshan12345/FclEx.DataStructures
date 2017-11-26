1e7f5f91c45cf6b0fdebe0b63c9a0eac7148c3b5
李经 <huoshan12345@hotmail.com>
2017-11-26 18:32:51 +0800

    xx

--- test/DataStructuresCSharpTest/Collections/DoublyCircularLinkedList/Reverse.cs
+++ test/DataStructuresCSharpTest/Collections/DoublyCircularLinkedList/Reverse.cs
@@ -39,7 +39,7 @@ namespace DataStructuresCSharpTest.Collections.DoublyCircularLinkedList
         [Theory]
         [InlineData(10, 0, 10)]
         [InlineData(10, 3, 3)]
-        [InlineData(10, 1, 10)]
+        [InlineData(10, 1, 1)]
         [InlineData(10, 10, 0)]
         [InlineData(10, 5, 5)]
         [InlineData(10, 0, 5)]
